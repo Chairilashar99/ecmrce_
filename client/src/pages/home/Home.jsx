53c47ac6db1223f421ae51bd8d1b99b139c5cb39
Chairilashar99 <asharchairil4@gmail.com>
2024-09-11 00:26:05 +0800

    feat: dummy product and slicing ui

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,27 +1,29 @@
 import { Box } from "@mui/material";
 import Appbar from "../../components/appbar/Appbar";
 import Footer from "../../components/footer/Footer";
+import Products from "../product/Products";
 
 const Home = () => {
-  return (
-    <div>
-      <Appbar />
+	return (
+		<div>
+			<Appbar />
 
-      <Box
-        sx={{
-          height: 935,
-          m: 2,
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-        }}
-      >
-        Products
-      </Box>
+			<Box
+				sx={{
+					minHeight: 935,
+					m: 2,
+					display: "flex",
+					alignItems: "center",
+					justifyContent: "center",
+					flexWrap: "wrap",
+					gap: 1,
+				}}>
+				<Products />
+			</Box>
 
-      <Footer />
-    </div>
-  );
+			<Footer />
+		</div>
+	);
 };
 
 export default Home;
