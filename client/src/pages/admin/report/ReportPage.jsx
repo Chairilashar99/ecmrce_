feb5fb5167fdfd0ae4c29e9b85cb8efc56014de3
Chairilashar99 <asharchairil4@gmail.com>
2024-09-28 19:31:13 +0800

    feat(admin): slicing ui admin-laporan

---
+++ client/src/pages/admin/report/ReportPage.jsx
@@ -0,0 +1,74 @@
+import { Box, Button, Typography } from "@mui/material";
+import AdminBar from "../components/appBar/AdminBar";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import { forwardRef, useEffect, useState } from "react";
+import ReportTable from "./ReportTable";
+
+const DateButton = forwardRef(({ value, onClick, className }, ref) => (
+	<Button variant="contained" color="primary" onClick={onClick} ref={ref}>
+		{value}
+	</Button>
+));
+
+const ReportPage = () => {
+	const [today, setToday] = useState(new Date());
+	const [startDate, setStartDate] = useState(new Date());
+	const [endDate, setEndDate] = useState(new Date());
+
+	useEffect(() => {
+		setToday(new Date());
+	}, []);
+
+	return (
+		<Box sx={{ height: "100vh" }}>
+			<AdminBar />
+
+			{/* Function */}
+			<Box sx={{ p: 2, display: "flex", justifyContent: "space-between" }}>
+				<Box
+					sx={{ display: "flex", width: 400, justifyContent: "space-evenly" }}>
+					{/* start */}
+					<Box sx={{ display: "flex", gap: 3, alignItems: "center" }}>
+						<Typography align="center" fontWeight="bold">
+							Dari
+						</Typography>
+						<DatePicker
+							closeOnScroll={true}
+							selected={startDate}
+							onChange={(date) => setStartDate(date)}
+							customInput={<DateButton />}
+							dateFormat="dd MM yyyy"
+							maxDate={today}
+						/>
+					</Box>
+
+					{/* end */}
+					<Box sx={{ display: "flex", gap: 3, alignItems: "center" }}>
+						<Typography align="center" fontWeight="bold">
+							Sampai
+						</Typography>
+						<DatePicker
+							closeOnScroll={true}
+							selected={endDate}
+							onChange={(date) => setEndDate(date)}
+							customInput={<DateButton />}
+							dateFormat="dd MM yyyy"
+							maxDate={today}
+						/>
+					</Box>
+				</Box>
+				<Button variant="contained" color="success">
+					Download
+				</Button>
+			</Box>
+
+			{/* Table */}
+			<Box sx={{ p: 2 }}>
+				<ReportTable />
+			</Box>
+		</Box>
+	);
+};
+
+export default ReportPage;
